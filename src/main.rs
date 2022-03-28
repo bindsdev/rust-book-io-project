33b57c03aa9d92c2aa4a282cdb530d555853afe9
akabinds <staticxxd@gmail.com>
2022-03-28 11:32:22 -0500

    Program Complete

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::{env, process};
+use rust_book_io_project::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Error parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = rust_book_io_project::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
